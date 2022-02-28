56517d4c188f1aca16ad2319f786427ed99f11b0
Martez Reed <martez.reed@greenreedtech.com>
2022-02-28 01:12:15 -0600

    Cleanup action

--- actions/get_project.py
+++ actions/get_project.py
@@ -3,4 +3,5 @@ from lib import action
 
 class GitlabProject(action.GitlabBaseAction):
     def run(self, project):
-        return self.projects.get(project)
+        #return self.projects.get(project)
+        return self
