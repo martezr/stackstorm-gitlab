66d7e3d5825c769aa35ea4b3c6dad92d558536e3
Martez Reed <martez.reed@greenreedtech.com>
2022-02-28 21:58:26 -0600

    Add ref variable

--- actions/trigger_project_pipeline.py
+++ actions/trigger_project_pipeline.py
@@ -4,9 +4,9 @@ from lib import action
 
 
 class TriggerProjectPipelineAction(action.GitlabBaseAction):
-    def run(self, project, trigger_token, variables):
+    def run(self, project, ref, trigger_token, variables):
         project_payload = self.gitlab.projects.get(project)
-        pipeline = project_payload.trigger_pipeline('main',trigger_token, variables=variables)
+        pipeline = project_payload.trigger_pipeline(ref,trigger_token, variables=variables)
         while pipeline.finished_at is None:
             pipeline.refresh()
             time.sleep(10)
