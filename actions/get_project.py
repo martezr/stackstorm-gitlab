1f10dcb1f90d0e479632ffdd9c680ab5c0a35f1f
Martez Reed <martez.reed@greenreedtech.com>
2022-03-01 19:49:21 -0600

    Add multi-tenancy

--- actions/get_project.py
+++ actions/get_project.py
@@ -2,6 +2,7 @@ from lib import action
 
 
 class GetProjectAction(action.GitlabBaseAction):
-    def run(self, project):
-        project_payload = self.gitlab.projects.get(project)
+    def run(self, project, credentials):
+        self.get_client(credentials)
+        project_payload = self.projects.get(project)
         return project_payload.attributes
