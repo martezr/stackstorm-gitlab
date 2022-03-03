9a624cb6869c763b4a6c1ebf3b77f9b8604bd08f
Martez Reed <martez.reed@greenreedtech.com>
2022-03-02 20:06:45 -0600

    Fix issue metadata

--- actions/create_project_issue.py
+++ actions/create_project_issue.py
@@ -2,10 +2,10 @@ from lib import action
 
 
 class CreateProjectIssueAction(action.GitlabBaseAction):
-    def run(self, project, credentials, url, token, verify_ssl):
+    def run(self, project, title, description, credentials, url, token, verify_ssl):
         self.get_client(credentials, url, token, verify_ssl)
         project_payload = self.gitlab.projects.get(project)
-        issue_payload = project_payload.issues.create({'title': 'I have a bug',
-                               'description': 'Something useful here.'})
+        issue_payload = project_payload.issues.create({'title': title,
+                               'description': description})
 
         return issue_payload.attributes
